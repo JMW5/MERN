081b6ad8494404a66da6471ec9164f1605a90791
jacquinwynn <jwynn@degdigital.com>
2019-02-18 01:20:09 -0500

    change client to frontend

--- client-new/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-//this file is basically to pull in all the other reducers
-
-import { combineReducers } from 'redux'
-import itemReducer from './itemReducer';
-
-export default combineReducers({
-    item: itemReducer
-})
--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use('/api/items', items)
 //Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static('client/build'))
+    app.use(express.static('frontend/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
 
